f4df8502ad1660832177991a1323c155a5ee03db
mackee <macopy123@gmail.com>
2022-09-03 14:58:48 +0900

    コメントアウトの仕方が間違っていました

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -3,6 +3,6 @@
 -- UPDATE id_generator SET id=2678400000 WHERE stub='a';
 -- ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
 
-# pstgresql
+-- postgresql
 DELETE FROM tenant WHERE id > 100;
 DELETE FROM visit_history WHERE created_at >= '1654041600';
